d751b6b6427bf56781f70eef953db8aad67be7b5
killgriff22 <kilgrif22033@gmail.com>
2024-03-07 10:20:15 -0500

    change channel

--- functions.py
+++ functions.py
@@ -5,12 +5,11 @@ from classes import *
 async def on_ready():
     print(f'{user.user.name} has connected to Discord!')
 
-
 @user.event
 async def on_message(message):
     if message.author == user.user:
         return
-    if not message.channel.id == 1164385876630052945:
+    if not message.channel.id == 1215317925049667594:
         return
     if message.content.startswith('!download'):
         command = message.content.split(' ')
@@ -30,3 +29,18 @@ async def on_message(message):
                 file = Soundcloud_path + Soundcloud.download_url(command[1])
                 await message.channel.send(f"Downloaded {command[1]}",files=[discord.File(file)])
                 os.remove(file)
+    elif message.content.startswith('!play'):
+        if len(queue) == 0:
+            command = message.content.split(' ')
+            if "https" in command[1]:
+                if "youtube" in command[1]:
+                    url = command[1]
+                    await message.channel.send(f"Playing {url}")
+                    author = message.author
+                    voice_channel = author.voice_channel
+                    vc = await user.join_voice_channel(voice_channel)
+                    player = await vc.create_ytdl_player(url)
+                    player.start()
+"""@tasks.loop(seconds=1)
+async def queue_loop():
+    if user."""
--- modules.py
+++ modules.py
@@ -2,8 +2,9 @@ from config import *
 import discord
 import requests
 import os
+from discord.ext import tasks
 user = discord.Client(intents=discord.Intents.all())
-
+queue = []
 Spotify_path = os.path.join(os.getcwd(), "Downloads/Spotify/")
 Youtube_path = os.path.join(os.getcwd(), "Downloads/Youtube/")
 Soundcloud_path = os.path.join(os.getcwd(), "Downloads/Soundcloud/")
