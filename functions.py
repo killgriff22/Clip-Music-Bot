dd56bf55d33bf4c4f6bf1345db22c5fa1e6441a6
killgriff22 <kilgrif22033@gmail.com>
2024-03-07 15:01:55 -0500

    test

--- functions.py
+++ functions.py
@@ -5,6 +5,7 @@ from classes import *
 async def on_ready():
     print(f'{user.user.name} has connected to Discord!')
 
+
 @user.event
 async def on_message(message):
     if message.author == user.user:
@@ -17,34 +18,38 @@ async def on_message(message):
             if "spotify" in command[1]:
                 await message.channel.send(f"downloading {command[1]}")
                 file = Spotify_path + Spotify.download_url(command[1])
-                await message.channel.send(f"Downloaded {command[1]}",files=[discord.File(file)])
+                await message.channel.send(f"Downloaded {command[1]}", files=[discord.File(file)])
                 os.remove(file)
             elif "youtube" in command[1]:
                 await message.channel.send(f"downloading {command[1]}")
                 file = Youtube_path + Youtube.download_url(command[1])
-                await message.channel.send(f"Downloaded {command[1]}",files=[discord.File(file)])
+                await message.channel.send(f"Downloaded {command[1]}", files=[discord.File(file)])
                 os.remove(file)
             elif "soundcloud" in command[1]:
                 await message.channel.send(f"downloading {command[1]}")
                 file = Soundcloud_path + Soundcloud.download_url(command[1])
-                await message.channel.send(f"Downloaded {command[1]}",files=[discord.File(file)])
+                await message.channel.send(f"Downloaded {command[1]}", files=[discord.File(file)])
                 os.remove(file)
         elif command[1].isdigit():
             for user_ in users.copy():
                 if user_.user == message.author:
                     await message.channel.send(f"downloading {user_.search_urls[int(command[1])]}")
-                    file = Spotify_path + Spotify.download_url(user_.search_urls[int(command[1])])
-                    await message.channel.send(f"Downloaded {user_.search_urls[int(command[1])]}",files=[discord.File(file)])
+                    file = Spotify_path + \
+                        Spotify.download_url(
+                            user_.search_urls[int(command[1])])
+                    await message.channel.send(f"Downloaded {user_.search_urls[int(command[1])]}", files=[discord.File(file)])
                     os.remove(file)
                     users.remove(user_)
                     break
-        else: #assume we want to search for something
+        else:  # assume we want to search for something
             users.append(User(message.author))
-            search = spotify.search(command[1])['tracks']['items']
-            users[-1].search_urls = [search[i]['external_urls']['spotify'] for i in range(len(search))]
-            await message.channel.send(f"""Search results for {command[1]}
-{"\n".join([f"{i} : {track['name']} - {track['artists'][0]['name']}" for i,track in enumerate(search)])}""")
-        
+            search = spotify.search(" ".join(command[1:]))['tracks']['items']
+            users[-1].search_urls = [search[i]['external_urls']['spotify']
+                                     for i in range(len(search))]
+            tracks = [f"{i} : {track['name']} - {track['artists'][0]['name']}" for i, track in enumerate(search)]
+            tracks = "\n".join(tracks)
+            await message.channel.send(f'Search results for {" ".join(command[1:])}'+"\n"+tracks)
+
     elif message.content.startswith('!play'):
         if len(queue) == 0:
             command = message.content.split(' ')
