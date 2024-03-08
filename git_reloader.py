c60daa392472d84133e55865d7135e3876f026ba
killgriff22 <kilgrif22033@gmail.com>
2024-03-08 07:58:05 -0500

    i give up

--- git_reloader.py
+++ git_reloader.py
@@ -1,14 +1,15 @@
 import subprocess
 import os
 import hashlib
+from tqdm import tqdm
 
-lastdir = [hashlib.md5(open(file).read().encode()) if os.path.isfile(file) else file for file in os.listdir()]
-process = subprocess.run(["bin/python", "main.py"])
+lastdir = [hashlib.md5(open(file).read().encode()) if os.path.isfile(file) else file for file in (os.listdir())]
+process = subprocess.Popen(["bin/python", "main.py"])
 while True:
-    new_hashes = [hashlib.md5(open(file).read().encode()) if os.path.isfile(file) else file for file in os.listdir()]
+    new_hashes = [hashlib.md5(open(file).read().encode()) if os.path.isfile(file) else file for file in (os.listdir())]
     if new_hashes != lastdir:
         lastdir = new_hashes
         process.kill()
-        process = subprocess.run(["bin/python", "main.py"])
+        process = subprocess.Popen(["bin/python", "main.py"])
     else:
         os.system("git pull")
