1e8b61445d51a880769da974c805b4ff6c3669bf
killgriff22 <kilgrif22033@gmail.com>
2024-03-08 07:52:51 -0500

    need to encode

--- git_reloader.py
+++ git_reloader.py
@@ -2,10 +2,10 @@ import subprocess
 import os
 import hashlib
 
-lastdir = [hashlib.md5(open(file).read()) if os.path.isfile(file) else file for file in os.listdir()]
+lastdir = [hashlib.md5(open(file).read().encode()) if os.path.isfile(file) else file for file in os.listdir()]
 process = subprocess.run("python main.py")
 while True:
-    new_hashes = [hashlib.md5(open(file).read()) if os.path.isfile(file) else file for file in os.listdir()]
+    new_hashes = [hashlib.md5(open(file).read().encode()) if os.path.isfile(file) else file for file in os.listdir()]
     if new_hashes != lastdir:
         lastdir = new_hashes
         process.kill()
