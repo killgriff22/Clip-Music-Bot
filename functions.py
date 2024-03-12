0a109c1837582037c0aa621c340720d10c64c1ce
killgriff22 <kilgrif22033@gmail.com>
2024-03-12 10:06:35 -0400

    forgot to do this

--- functions.py
+++ functions.py
@@ -24,7 +24,7 @@ async def on_message(message):
                 length = len(f.readlines())
             await message.channel.send(f"Added {add} to statuses ({length})")
             return
-    if not message.channel.id == 1215317925049667594:
+    if not message.channel.id in [1215317925049667594,1164386048407781457]:
         return
     if not message.content.startswith('!'):
         return
@@ -60,7 +60,7 @@ async def on_message(message):
                                     user_.search_urls[int(command[1])])
                         elif "youtu" in url:
                             file = Youtube_path + \
-                                    Youtube.download_url(url)
+                                Youtube.download_url(url)
                         await message.channel.send(f"Downloaded {url}", files=[discord.File(file)])
                         os.remove(file)
                         users.remove(user_)
@@ -73,7 +73,8 @@ async def on_message(message):
                 users[-1].search_urls = [search[i]['external_urls']['spotify']
                                          for i in range(len(search))]
                 # format the search results
-                tracks = [f"{i} : {track['name']} - {track['artists'][0]['name']}{f' - album' if 'album' in track['external_urls']['spotify'] else f' - playlist' if 'playlist' in track['external_urls']['spotify'] else ''}" for i, track in enumerate(search)]
+                tracks = [f"{i} : {track['name']} - {track['artists'][0]['name']}{f' - album' if 'album' in track['external_urls']['spotify']
+                                                                                  else f' - playlist' if 'playlist' in track['external_urls']['spotify'] else ''}" for i, track in enumerate(search)]
                 tracks = "\n".join(tracks)
                 await message.channel.send(f'Search results for {" ".join(command[1:])}'+"\n"+tracks)
         case '!list' | '!l' | '!album' | '!a' | '!playlist' | '!pl':
@@ -85,7 +86,8 @@ async def on_message(message):
                         for track in album['tracks']['items']:
                             users[-1].search_urls.append(
                                 track['external_urls']['spotify'])
-                        tracks = [f"{i} : {track['name']} - {track['artists'][0]['name']}" for i, track in enumerate(album['tracks']['items'])]
+                        tracks = [f"{i} : {track['name']} - {track['artists'][0]['name']
+                                                             }" for i, track in enumerate(album['tracks']['items'])]
                         tracks = "\n".join(tracks)
                         await message.channel.send(f'Album {album["name"]}'+"\n"+tracks)
                     elif 'playlist' in split[1]:
@@ -94,19 +96,21 @@ async def on_message(message):
                         for track in playlist['tracks']['items']:
                             users[-1].search_urls.append(
                                 track['track']['external_urls']['spotify'])
-                        tracks = [f"{i} : {track['track']['name']} - {track['track']['artists'][0]['name']}" for i, track in enumerate(playlist['tracks']['items'])]
+                        tracks = [f"{i} : {track['track']['name']} - {track['track']['artists']
+                                                                      [0]['name']}" for i, track in enumerate(playlist['tracks']['items'])]
                         tracks = "\n".join(tracks)
                         await message.channel.send(f'Playlist {playlist["name"]}'+"\n"+tracks)
                 elif "youtu" in split[1]:
                     await message.channel.send("Not Implemented")
                     return
-                    playlist:Playlist = Playlist(split[1])
+                    playlist: Playlist = Playlist(split[1])
                     users.append(User(message.author))
                     for video in playlist.videos:
                         users[-1].search_urls.append(
                             video.watch_url
                         )
-                    tracks = [f"{i} : {track.title} - {track.author}" for i, track in enumerate(playlist.videos)]
+                    tracks = [f"{i} : {track.title} - {track.author}" for i,
+                              track in enumerate(playlist.videos)]
                     tracks = "\n".join(tracks)
                     await message.channel.send(f'Playlist {playlist.title}'+"\n"+tracks)
                 elif "soundcloud" in split[1]:
@@ -126,10 +130,8 @@ async def on_message(message):
             author = message.author
             voice_channel = author.voice_channel
             vc = await user.join_voice_channel(voice_channel)
-            command = message.content.split(' ')
-            url = command[1]
-            await message.channel.send(f"Playing {url}")
-            vc.play(discord.FFmpegPCMAudio(executable="~/.spotdl/ffmpeg", source="Downloads/Spotify/The Living Tombstone - My Ordinary Life.mp3"))
+            vc.play(discord.FFmpegPCMAudio(executable="~/.spotdl/ffmpeg",
+                    source="Downloads/Spotify/The Living Tombstone - My Ordinary Life.mp3"))
 
 
 @tasks.loop(seconds=1)
