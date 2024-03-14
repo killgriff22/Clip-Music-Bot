9f9663584e22f19e1c5757a1568fb085aa09cdd7
killgriff22 <kilgrif22033@gmail.com>
2024-03-14 10:06:04 -0400

    bring downloader.py up to date

--- downloader.py
+++ downloader.py
@@ -1,8 +1,8 @@
 import sys
 import os
-from classes import *
+from functions import *
 args = sys.argv
-queue = eval(open("queue.txt").read())
+queue = read_queue()
 if "spotify" in args[1]:
     file = Spotify_path + Spotify.download_url(args[1])
 elif "youtu" in args[1]:
@@ -10,4 +10,4 @@ elif "youtu" in args[1]:
 elif "soundcloud" in args[1]:
     file = Soundcloud_path + Soundcloud.download_url(args[1])
 queue.append(file)
-open("queue.txt","w").write(str(queue))
+update_queue()
